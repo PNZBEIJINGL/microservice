c2b8690d9026dd8705cce27ea17cbe3e839ff3f6
liuyong <liuyong@startimes.com.cn>
2019-09-18 19:48:16 +0800

    19:47 Spring boot Admin 测试

--- ms-framework/src/main/java/com/ms/framework/registry/SpringbootApplication.java
+++ ms-framework/src/main/java/com/ms/framework/registry/SpringbootApplication.java
@@ -1,5 +1,6 @@
 package com.ms.framework.registry;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@EnableAdminServer  //启动Spring Boot Admin服务器端扫描，通过http://localhost:8080 查看应用程序主界面
 @SpringBootApplication
 public class SpringbootApplication {
 
-    @RequestMapping("/")
+    @RequestMapping("/welcome")
     public String index() {
         return "Hello World ,This is index";
     }
---
+++ ms-simple/src/main/java/com/ms/simple/main/AdminClientApplication.java
@@ -0,0 +1,15 @@
+package com.ms.simple.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClientApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AdminClientApplication.class, args);
+
+    }
+
+}
