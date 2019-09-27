4726e3188583d0c1d542a26c8ac4604787d25e38
liuyong <liuyong@startimes.com.cn>
2019-09-27 17:57:46 +0800

    2344 HTTP 同步测试

---
+++ ms-customer/src/main/java/com/ms/customer/controller/TestController.java
@@ -0,0 +1,46 @@
+package com.ms.customer.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+/**
+ * 只是一个测试类
+ */
+@RestController
+public class TestController {
+
+    /**
+     * RequestParam从请求参数中获取指定名称的参数
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping("/version")
+    public String getVersion(@RequestParam("name") String name) {
+        return "version: " + "1.0.0" + " request=" + name;
+    }
+
+    /**
+     * PathVariable 从请求路径中获取指定名称的参数
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping("/date ")
+    public String say2(@PathVariable("name") String name) {
+        return "date: " + new Date() + " request=" + name;
+    }
+
+    /**
+     * 从请求头重获取制定名称的参数
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping("/info")
+    public String say3(@RequestHeader("name") String name) {
+        return "info: " + "test me" + " request=" + name;
+    }
+
+}
